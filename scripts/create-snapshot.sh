cf62d29563d5b00a0b3b95204c1cd115e30f4668
hadley31 <nick.hadley31@gmail.com>
2024-07-15 17:49:07 -0400

    feat: add create-snapshot script

---
+++ scripts/create-snapshot.sh
@@ -0,0 +1,5 @@
+SERVICE_NAME=${1:-"cs2-server"}
+VOLUME_ID=$(aws ec2 describe-volumes --filters "Name=tag:aws:ecs:serviceName,Values=$SERVICE_NAME" --query 'Volumes[*].VolumeId' --output text)
+
+echo "Creating snapshot for volume $VOLUME_ID"
+aws ec2 create-snapshot --volume-id $VOLUME_ID --description "Counter-Strike 2 Install Snapshot" --tag-specifications "ResourceType=snapshot,Tags=[{Key=Name,Value=cs2-install}]"
