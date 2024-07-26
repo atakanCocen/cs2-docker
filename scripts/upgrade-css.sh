cf871c458872e617ae62f75ddc644d6fe30d16b5
hadley31 <nick.hadley31@gmail.com>
2024-07-25 20:28:36 -0400

    chore: update css upgrade script to replace text in the Dockerfile

--- scripts/upgrade-css.sh
+++ scripts/upgrade-css.sh
@@ -1,8 +1,9 @@
-CS_SHARP_URL=$(curl -L \
+CS_SHARP_URL=$(curl -sSL \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/roflmuffin/CounterStrikeSharp/releases/latest | jq -c '.assets.[] | select( .name | test("^counterstrikesharp-with-runtime-build-[0-9]+-linux-[a-zA-Z0-9]+.zip$")) | .browser_download_url')
 
-echo $CS_SHARP_URL
+ARG_REPLACE_STR="ARG CS_SHARP_URL="
+REPLACEMENT_STR="${ARG_REPLACE_STR}${CS_SHARP_URL}"
 
-# TODO update Dockerfile with sed
+sed -i '' "s|${ARG_REPLACE_STR}.*|${REPLACEMENT_STR}|" Dockerfile
