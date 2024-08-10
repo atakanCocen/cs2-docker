4f823ec1170c65c43dac752c83ec2edff354000c
hadley31 <nick.hadley31@gmail.com>
2024-08-10 12:56:54 -0400

    refactor: move volume name to variable:

--- scripts/create-snapshot.sh
+++ scripts/create-snapshot.sh
@@ -1,5 +1,6 @@
 SERVICE_NAME=${1:-"cs2-server"}
+VOLUME_NAME=cs2-install
 VOLUME_ID=$(aws ec2 describe-volumes --filters "Name=tag:aws:ecs:serviceName,Values=$SERVICE_NAME" --query 'Volumes[*].VolumeId' --output text)
 
 echo "Creating snapshot for volume $VOLUME_ID"
-aws ec2 create-snapshot --volume-id $VOLUME_ID --description "Counter-Strike 2 Install Snapshot" --tag-specifications "ResourceType=snapshot,Tags=[{Key=Name,Value=cs2-install}]"
+aws ec2 create-snapshot --volume-id $VOLUME_ID --description "Counter-Strike 2 Install Snapshot" --tag-specifications "ResourceType=snapshot,Tags=[{Key=Name,Value=$VOLUME_NAME}]"
